0aab97eff0bf3d2f4e8754ecbc321ee992e0f052
gsanto1973 <93636042+gsanto1973@users.noreply.github.com>
2024-02-23 19:44:52 -0500

    Commit 02

--- Accounting.js
+++ Accounting.js
@@ -11,6 +11,14 @@ app.get('/generic', (req, res) => {
     res.send('Hola Giorda - generic');
 })
 
+app.get('/heroesdc', (req, res) => {
+    res.send('Hola Giorda - heroesDC');
+})
+
+app.get('/heroesmarvel', (req, res) => {
+    res.send('Hola Giorda - heroesMarvel');
+})
+
 app.listen(4545, () => {
     console.log(`Example app listening on port 4545`)
 })
